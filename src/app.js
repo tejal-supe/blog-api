f25036fbb1cd9ef7f66c778bf6078c9c16df60f1
tejal-supe <supetejalc@gmail.com>
2025-09-05 11:11:47 +0530

    Folder structure and basic setup

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+
+const app = express();
+app.use(
+  cors({
+    origin: "",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+app.use(cookieParser());
